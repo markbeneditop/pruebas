9ad312626ede18d49f2357b06f7fb6e2a6f77530
markbeneditop <marcbp9@hotmail.com>
2014-07-03 19:42:28 +0200

    prova

---
+++ StockLlantas/src/test/CalculoAlmacenTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculoAlmacenTest {
+	
+	
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
